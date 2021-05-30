370f1fe299b00f12fe621db2f04516ca55094b0b
burkaydurdu <burkaydurdu@outlook.com>
2021-05-31 01:30:07 +0300

    Build test structure

---
+++ src/main/kotlin/com/pow/controllers/ApplicationController.kt
@@ -0,0 +1,17 @@
+package com.pow.controllers
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
+
+@Controller("/")
+open class ApplicationController {
+
+    @Get("/")
+    @Secured(SecurityRule.IS_ANONYMOUS)
+    open fun index(): HttpResponse<String> {
+        return HttpResponse.ok("Simple Mock API")
+    }
+}
--- src/test/kotlin/com/pow/MockTest.kt
+++ src/test/kotlin/com/pow/MockTest.kt
@@ -3,11 +3,25 @@ package com.pow
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.kotest.annotation.MicronautTest
 import io.kotest.core.spec.style.StringSpec
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.annotation.Client
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
 
 @MicronautTest
-class MockTest(private val application: EmbeddedApplication<*>) : StringSpec({
+class MockTest(private val application: EmbeddedApplication<*>,
+               @Client("/") private val client: RxHttpClient) : StringSpec({
 
     "test the server is running" {
         assert(application.isRunning)
+    }
+
+    "test the base route" {
+        val request = HttpRequest.GET<String>("/")
+        val body = client.toBlocking().retrieve(request)
+
+        assertNotNull(body)
+        assertEquals("Simple Mock API", body);
     }
 })
